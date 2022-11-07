076faf936397e331637e4f16f5b6faa285821d8b
Leonardo Aver Dal Mass <leoaverdalmass@gmail.com>
2022-11-07 03:18:20 -0300

    First basic implementation using TanStack React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { FormEvent, useEffect, useRef, useState } from "react"
+import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 interface ToDo {
   id: number,
@@ -6,52 +7,95 @@ interface ToDo {
   done: boolean
 }
 
+const queryClient = new QueryClient()
 const API_URL = import.meta.env.DEV ? "http://localhost:3000" : import.meta.env.VITE_API_URL
 
 function App() {
-  const newTodoInputRef = useRef<HTMLInputElement>(null)
-  const [todos, setTodos] = useState<ToDo[]>([])
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Todos />
+    </QueryClientProvider>
+  )
+}
 
-  useEffect(() => {
-    fetch(`${API_URL}/todos`)
-      .then(res => res.json())
-      .then((data: ToDo[]) => setTodos(data))
-  }, [])
+function Todos() {
+  const [newToDo, setNewToDo] = useState<string>("")
+  const queryClient = useQueryClient()
+
+  const query = useQuery<ToDo[]>(
+    { 
+      queryKey: ['todos'], 
+      queryFn: () => {
+        const promise = fetch(`${API_URL}/todos`)
+          .then(res => res.json())
+        return promise
+      },
+      refetchOnWindowFocus: false
+    }
+  )
+
+  const createMutation = useMutation({
+    mutationFn: (newTodo: Partial<ToDo>) => {
+      const reqOptions: RequestInit = {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newTodo)
+      }
+
+      const promise = fetch(`${API_URL}/todos`, reqOptions)
+        .then(res => res.json())
+
+      return promise
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] })
+      setNewToDo("")
+    },
+    onError: () => {
+      alert('An error occurred while trying to add To Do')
+    }
+  })
+
+  const deleteMutation = useMutation({
+    mutationFn: (id: number) => {
+      const reqOptions: RequestInit = {
+        method: "DELETE"
+      }
+
+      const promise = fetch(`${API_URL}/todos/${id}`, reqOptions)
+        .then(res => res.json())
+
+      return promise
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] })
+    },
+    onError: () => {
+      alert('An error occurred while trying to delete To Do')
+    }
+  })
 
   function handleCreateToDo(event: FormEvent){
     event.preventDefault()
-    
-    const value = newTodoInputRef.current?.value
-    if(!value) return
 
     const payload: Partial<ToDo> = {
-      todo: value,
+      todo: newToDo,
       done: false
     }
 
-    const reqOptions: RequestInit = {
-      method: "POST",
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(payload)
-    }
-
-    fetch(`${API_URL}/todos`, reqOptions)
-      .then(res => res.json())
-      .then((data: ToDo) => setTodos(curr => [data, ...curr]))
-      .catch(() => alert('Something went wrong when trying to create To Do'))
-
-    newTodoInputRef.current.value = ""
+    createMutation.mutate(payload)
   }
 
   function handleDeleteToDo(id: number){
-    const reqOptions: RequestInit = {
-      method: "DELETE"
-    }
+    deleteMutation.mutate(id)
+  }
 
-    fetch(`${API_URL}/todos/${id}`, reqOptions)
-      .then(res => res.json())
-      .then(() => setTodos(curr => curr.filter(x => x.id !== id)))
-      .catch(() => alert('Something went wrong when trying to delete To Do'))
+  if(query.isLoading){
+    return <div>Loading...</div>
+  }
+
+  if(query.isError){
+    return <div>An error occurred while fetching data</div>
   }
 
   return (
@@ -60,13 +104,13 @@ function App() {
 
       <form onSubmit={handleCreateToDo}>
         <label htmlFor="newTodoInput">New To Do:</label>
-        <input id="newTodoInput" ref={newTodoInputRef}/>
-        <button type="submit">Add</button>
+        <input id="newTodoInput" value={newToDo} onChange={e => setNewToDo(e.target.value)}/>
+        <button type="submit" disabled={createMutation.isLoading}>Add</button>
       </form>
 
       <ul>
         {
-          todos.map(todo => <li key={todo.id}>
+          query.data.map(todo => <li key={todo.id}>
             <span>{todo.todo}</span>
             <button type="button" onClick={() => handleDeleteToDo(todo.id)}>X</button>
           </li>)
