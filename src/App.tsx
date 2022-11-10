ac1f788e351552aa3feeaa3ef293c0caccd66f78
Leonardo Aver Dal Mass <leoaverdalmass@gmail.com>
2022-11-10 02:02:49 -0300

    Added Tailwind CSS package and initial styling. Added done todo logic and update mutation.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
-import { FormEvent, useEffect, useRef, useState } from "react"
+import { FormEvent, useMemo, useState } from "react"
 import { useQuery, useMutation, useQueryClient, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import './styles/global.css'
 
 interface ToDo {
   id: number,
@@ -7,12 +9,12 @@ interface ToDo {
   done: boolean
 }
 
-const queryClient = new QueryClient()
+const baseQueryClient = new QueryClient()
 const API_URL = import.meta.env.DEV ? "http://localhost:3000" : import.meta.env.VITE_API_URL
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={baseQueryClient}>
       <Todos />
     </QueryClientProvider>
   )
@@ -30,9 +32,20 @@ function Todos() {
           .then(res => res.json())
         return promise
       },
-      refetchOnWindowFocus: false
+
     }
   )
+
+  const todos = useMemo(() => {
+    if(query.isLoading || !query.data) return []
+    return query.data.filter(x => !x.done)
+  }, [query.data])
+
+  const doneTodos = useMemo(() => {
+    if(query.isLoading || !query.data) return []
+    return query.data.filter(x => x.done)
+  }, [query.data])
+
 
   const createMutation = useMutation({
     mutationFn: (newTodo: Partial<ToDo>) => {
@@ -48,11 +61,32 @@ function Todos() {
       return promise
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['todos'] })
       setNewToDo("")
+      queryClient.invalidateQueries(['todos'])
     },
     onError: () => {
       alert('An error occurred while trying to add To Do')
+    }
+  })
+
+  const updateMutation = useMutation({
+    mutationFn: (todo: Partial<ToDo>) => {
+      const reqOptions: RequestInit = {
+        method: "PUT",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(todo)
+      }
+
+      const promise = fetch(`${API_URL}/todos/${todo.id}`, reqOptions)
+        .then(res => res.json())
+
+      return promise
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(['todos'])
+    },
+    onError: () => {
+      alert('An error occurred while trying to update To Do')
     }
   })
 
@@ -68,14 +102,14 @@ function Todos() {
       return promise
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['todos'] })
+      queryClient.invalidateQueries(['todos'])
     },
     onError: () => {
       alert('An error occurred while trying to delete To Do')
     }
   })
 
-  function handleCreateToDo(event: FormEvent){
+  function handleCreateTodo(event: FormEvent){
     event.preventDefault()
 
     const payload: Partial<ToDo> = {
@@ -86,36 +120,108 @@ function Todos() {
     createMutation.mutate(payload)
   }
 
-  function handleDeleteToDo(id: number){
+  function handleUpdateTodo(todo: ToDo){
+    const payload = {
+      ...todo,
+      done: !todo.done
+    }
+
+    updateMutation.mutate(payload)
+  }
+
+  function handleDeleteTodo(id: number){
     deleteMutation.mutate(id)
   }
 
   if(query.isLoading){
-    return <div>Loading...</div>
+    return <div className="h-screen flex items-center justify-center text-4xl">
+      Loading...
+    </div>
   }
 
   if(query.isError){
-    return <div>An error occurred while fetching data</div>
+    return <div className="h-screen flex items-center justify-center text-4xl">
+      An error occurred while fetching data
+    </div>
   }
 
   return (
-    <div>
-      <h1>Simple To Do</h1>
+    <div className="p-8 min-h-screen flex flex-col items-center gap-y-8">
+      <h1 className="text-4xl font-semibold text-teal-300">Simple To Do</h1>
 
-      <form onSubmit={handleCreateToDo}>
-        <label htmlFor="newTodoInput">New To Do:</label>
-        <input id="newTodoInput" value={newToDo} onChange={e => setNewToDo(e.target.value)}/>
-        <button type="submit" disabled={createMutation.isLoading}>Add</button>
+      <form onSubmit={handleCreateTodo} className="flex flex-col items-center gap-1">
+        <label className="self-start"
+          htmlFor="newTodoInput">
+          New Todo:
+        </label>
+        <input className="text-black py-0.5 px-1 rounded focus:outline-none"
+          id="newTodoInput" 
+          value={newToDo} 
+          onChange={e => setNewToDo(e.target.value)}
+          placeholder="Task description"
+        />
+        <button className="px-8 py-0.5 rounded bg-teal-300 hover:bg-teal-200 text-xl text-black duration-200 disabled:bg-teal-900" 
+          type="submit" 
+          disabled={createMutation.isLoading}>
+          Create
+        </button>
       </form>
 
-      <ul>
+      <div className="flex gap-4 w-full">
+        <div className="bg-white w-full h-px self-center mt-2"></div>
+        <h2 className="text-3xl">Todos</h2>
+        <div className="bg-white w-full h-px self-center mt-2"></div>
+      </div>
+
+      <ul className="p-1 max-w-3xl bg-zinc-800 flex flex-col gap-y-1 text-xl">
         {
-          query.data.map(todo => <li key={todo.id}>
+          todos.map(todo => <li key={todo.id} className="bg-zinc-900 flex flex-col p-2 gap-y-1">
             <span>{todo.todo}</span>
-            <button type="button" onClick={() => handleDeleteToDo(todo.id)}>X</button>
+            <div className="text-base flex self-end gap-x-1">
+              <button className="text-teal-400 hover:text-teal-300"
+                type="button" 
+                onClick={() => handleUpdateTodo(todo)}>
+                Mark as done
+              </button>
+              <button className="text-red-400 hover:text-red-500"
+                type="button" 
+                onClick={() => handleDeleteTodo(todo.id)}>
+                Delete
+              </button>
+            </div>
           </li>)
         }
       </ul>
+      
+      <div className="flex gap-4 w-full">
+        <div className="bg-white w-full h-px self-center mt-2"></div>
+        <h2 className="text-3xl">Done</h2>
+        <div className="bg-white w-full h-px self-center mt-2"></div>
+      </div>
+
+      {
+        doneTodos.length > 0 ?
+        <ul className="max-w-3xl flex flex-col gap-y-1 text-xl">
+          {
+            doneTodos.map(todo => <li key={todo.id} className="bg-zinc-800 flex flex-col p-2 gap-y-1 rounded-xl">
+              <span>{todo.todo}</span>
+              <div className="text-base flex self-end gap-x-1">
+                <button className="text-teal-400 hover:text-teal-300"
+                  type="button" 
+                  onClick={() => handleUpdateTodo(todo)}>
+                  Mark as undone
+                </button>
+                <button className="text-red-400 hover:text-red-500"
+                  type="button" 
+                  onClick={() => handleDeleteTodo(todo.id)}>
+                  Delete
+                </button>
+              </div>
+            </li>)
+          }
+        </ul> :
+        <span className="text-base">There is no Todo Item marked as done</span>
+      }
     </div>
   )
 }
